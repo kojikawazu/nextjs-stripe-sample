ef02c97ad49652ea869d6520a4a1630eb11345ad
koji kawazu <nanairo7.daydream21@gmail.com>
2024-05-16 23:23:37 +0900

    ビルドエラーの修正

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -39,13 +39,13 @@ const LesonDetailPage = async ({
         await getDetailLesson(params.id, supabase),
         await getPremiumContent(params.id, supabase),
     ]);
-    const videoId = extractYouTubeVideoId(video?.video_url) as string;
+    const videoId = video?.video_url ? extractYouTubeVideoId(video.video_url) : null;
 
     return (
         <div className="w-full max-w-3xl mx-auto py-16 px-8">
             <h1 className="text-3xl mb-6">{lesson?.title}</h1>
             <p className="mb-8">{lesson?.description}</p>
-            <YouTubeEmbed height={400} videoid={videoId}/>
+            {videoId && <YouTubeEmbed height={400} videoid={videoId} />}
         </div>
     );
 }
